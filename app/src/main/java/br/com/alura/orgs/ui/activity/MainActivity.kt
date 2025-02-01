f70a04609a91ef081dab4b7f6567d71804580681
al1neferreira <alinfst@gmail.com>
2025-02-01 13:51:59 -0300

    Remove comments

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,7 +2,6 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
 import br.com.alura.orgs.model.Produto
@@ -14,12 +13,7 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, manga e maçã"
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99"
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
                 Produto(nome = "teste",
@@ -35,7 +29,7 @@ class MainActivity : Activity() {
                         valor = BigDecimal("39.99")
                 ),
         ))
-//        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 
 }
