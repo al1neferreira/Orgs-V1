fc37c7ae135735ba88ae660ea32a67615342e6ac
al1neferreira <alinfst@gmail.com>
2025-02-03 19:34:33 -0300

    Add AppCompatActivity

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProduto.kt
@@ -0,0 +1,10 @@
+package br.com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
+
+class FormularioProduto :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+
+}
