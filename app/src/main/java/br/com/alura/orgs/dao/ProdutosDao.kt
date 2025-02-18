8200a8a7489f2d103a547d6b4d582e615e3b2184
al1neferreira <alinfst@gmail.com>
2025-02-18 18:58:54 -0300

    Add fonts and image

--- app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -1,21 +1,28 @@
 package br.com.alura.orgs.dao
 
 import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
     fun adiciona(produto: Produto) {
 
         Companion.produtos.add(produto)
-  }
+    }
 
 
-    fun buscaTodos() : List<Produto> {
+    fun buscaTodos(): List<Produto> {
         return Companion.produtos.toList()
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de frutas",
+                descricao = "Laranja, manga e uva",
+                valor = BigDecimal("19.83")
+            )
+        )
     }
 
 }
