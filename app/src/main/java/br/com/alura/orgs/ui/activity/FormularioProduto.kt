1898d03da1e9008d65c71f492beab8fb7245fb69
al1neferreira <alinfst@gmail.com>
2025-03-31 19:07:24 -0300

    Alert Dialog refactor

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProduto.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProduto.kt
@@ -1,14 +1,12 @@
 package br.com.alura.orgs.ui.activity
 
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.orgs.dao.ProdutosDao
 import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding
-import br.com.alura.orgs.databinding.FormularioImagemBinding
 import br.com.alura.orgs.extensions.carregarImagem
 import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.ui.dialog.FormularioImageDialog
 import java.math.BigDecimal
 
 class FormularioProduto :
@@ -25,32 +23,16 @@ class FormularioProduto :
         setContentView(view)
         configuraBotaoSalvar()
 
-
-
         binding.activityFormularioProdutoImagem.setOnClickListener {
-            val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
-            bindingFormularioImagem.formularioImagemBotaoCarregar.setOnClickListener {
-             url = bindingFormularioImagem.formularioImagemUrl.text.toString()
-                bindingFormularioImagem.formularioImagemImageview.carregarImagem(url)
-            }
-
-
-            AlertDialog.Builder(this)
-                .setView(bindingFormularioImagem.root)
-                .setPositiveButton("Confirmar") { _, _ ->
-                    val url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+            FormularioImageDialog(this)
+                .mostrarDialog{ imagem ->
+                    url = imagem
                     binding.activityFormularioProdutoImagem.carregarImagem(url)
+                    
                 }
-                .setNegativeButton("Cancelar") { _, _ ->
-                }
-                .show()
         }
     }
 
-    fun isLoadingVisible(isVisible: Boolean) {
-        val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
-        bindingFormularioImagem.loadingImage.visibility = if (isVisible) View.VISIBLE else View.GONE
-    }
 
     private fun configuraBotaoSalvar() {
         val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
